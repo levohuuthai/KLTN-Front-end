8a3912dabe02c4c603faac446bbe2baf342ee116
levohuuthai <levohuuthai1@gmail.com>
2022-05-25 21:39:20 +0700

    update

--- src/features/Auth/components/LoginGoogleSlice/LoginGoogleSlice.js
+++ src/features/Auth/components/LoginGoogleSlice/LoginGoogleSlice.js
@@ -2,17 +2,17 @@ import authAPI from "api/authAPI";
 import Cookies from "js-cookie";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-// export const signin = createAsyncThunk(
-//   "auth/login_facebook",
-//   async (payload) => {
-//     const data = await authAPI.login_facebook(payload);
-//     localStorage.setItem("user", JSON.stringify(data.data.user));
-//     Cookies.set("token", data.data.accessToken);
-//     Cookies.set("refreshToken", data.data.refreshToken);
-//     console.log(data.data);
-//     return data.data.user;
-//   }
-// );
+export const signinGoogle = createAsyncThunk(
+  "auth/add_login_google",
+  async (payload) => {
+    const data = await authAPI.add_login_google(payload);
+    localStorage.setItem("user", JSON.stringify(data.data.user));
+    Cookies.set("token", data.data.accessToken);
+    Cookies.set("refreshToken", data.data.refreshToken);
+    console.log(data.data);
+    return data.data.user;
+  }
+);
 
 const userSlice = createSlice({
   name: "user",
@@ -21,7 +21,7 @@ const userSlice = createSlice({
   },
   reducers: {},
   extraReducers: {
-    [signin.fulfilled]: (state, action) => {
+    [signinGoogle.fulfilled]: (state, action) => {
       state.current = action.payload; //update trên store
     },
   },
