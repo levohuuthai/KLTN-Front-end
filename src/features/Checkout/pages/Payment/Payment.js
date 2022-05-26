f4584f94ff03714cfccb6d1ff7a6fd081478457e
levohuuthai <levohuuthai1@gmail.com>
2022-05-27 02:34:35 +0700

    update

--- src/features/Checkout/pages/Payment/Payment.js
+++ src/features/Checkout/pages/Payment/Payment.js
@@ -251,8 +251,31 @@ function Payment(props) {
       fetchCheckoutPayment();
     }
   };
-
+  console.log(
+    state.dataCart.map((data) => {
+      return {
+        productDetailId: data.product.productDetailId,
+        quantity: data.product.quantity,
+        priceAfter: data.product.priceAfter,
+        priceBefore: data.product.priceBefore,
+      };
+    })
+  );
+  console.log(totalMoney);
+  console.log(dataAddress);
   useEffect(() => {
+    console.log(
+      state.dataCart.map((data) => {
+        return {
+          productDetailId: data.product.productDetailId,
+          quantity: data.product.quantity,
+          priceAfter: data.product.priceAfter,
+          priceBefore: data.product.priceBefore,
+        };
+      })
+    );
+    console.log(totalMoney);
+    console.log(dataAddress);
     if (new URL(document.location).searchParams.get("code") !== null) {
       console.log(
         state.dataCart.map((data) => {
